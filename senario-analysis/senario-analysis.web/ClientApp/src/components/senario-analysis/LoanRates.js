20dfce5a096e9f720d11a9ef4ad9503b604e7702
Mattias Lundh <Mattiasanderslundh@gmail.com>
2020-02-25 22:15:19 +0100

    added senario components

--- senario-analysis/senario-analysis.web/ClientApp/src/components/senario-analysis/LoanRates.js
+++ senario-analysis/senario-analysis.web/ClientApp/src/components/senario-analysis/LoanRates.js
@@ -17,10 +17,26 @@ export class LoanRates extends Component {
                         <div class="card-body">
                             <div class="form-check">
                                 <label className="badge badge-dark">Type</label><br />
-                                <input id="fixed" className="form-check-input" name="rate-builder-type" type="radio" value="fixed" />
-                                <label for="fixed" className="form-check-label">Fixed</label><br />
-                                <input id="floating" className="form-check-input" name="rate-builder-type" type="radio" value="floating" />
-                                <label for="floating" className="form-check-label">Floating</label>
+                                <div>
+                                    <input id="fixed" className="form-check-input" name="rate-builder-type" type="radio" value="fixed" />
+                                    <label for="fixed" className="form-check-label">Fixed</label><br />
+
+                                    <input id="floating" className="form-check-input" name="rate-builder-type" type="radio" value="floating" />
+                                    <label for="floating" className="form-check-label">Floating</label><br />
+                                </div>
+
+                                <label className="badge badge-dark">Term</label><br />
+                                <input id="years" className="form-check-input" type="number" />
+                                <label for="years" className="form-check-label">Years</label><br /><br />
+
+                                <label className="badge badge-dark">Proportion</label><br />
+                                <input id="proportion" className="form-check-input" type="number" min="1" max="100" />
+                                <label for="proportion" className="form-check-label">Proportion</label><br />
+
+                                <label className="badge badge-dark">Rate</label><br />
+                                <input id="rate" className="form-check-input" type="number" min="0" step="0.01" />
+                                <label for="rate" className="form-check-label">Rate</label><br />
+
                             </div>
                         </div>
                     </div>
